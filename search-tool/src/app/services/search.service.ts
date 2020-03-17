a831fa24734577118f20afdfd52aa4d84e3d1f90
momothereal <momothereal.mc@gmail.com>
2020-03-16 22:17:22 -0400

    Hostname

--- search-tool/src/app/services/search.service.ts
+++ search-tool/src/app/services/search.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {SearchResult} from "../models/search-result.model";
 import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable()
 export class SearchService {
@@ -9,6 +10,7 @@ export class SearchService {
   }
 
   search(term: string): Observable<SearchResult[]> {
-    return this.http.get<SearchResult[]>("http://localhost:8080/search", {params: {"term": term}});
+    const host = environment.production ? "http://159.203.3.119:8080" : "http://localhost:8080";
+    return this.http.get<SearchResult[]>(host + "/search", {params: {"term": term}});
   }
 }
