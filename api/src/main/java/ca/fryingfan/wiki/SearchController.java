eae3153a0030343c92d0216b341c1da1ff2ab876
momothereal <momothereal.mc@gmail.com>
2020-03-16 22:13:01 -0400

    Allow all origins

--- api/src/main/java/ca/fryingfan/wiki/SearchController.java
+++ api/src/main/java/ca/fryingfan/wiki/SearchController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 public class SearchController {
 
